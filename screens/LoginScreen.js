a9dcc4387ce159031caf209b156b4caf2219175f
fmfahath <fmfahath.dev@gmail.com>
2024-01-30 18:01:21 +0530

    loginScreen : sttusbar added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function LoginScreen() {
     return (
         <View className='bg-white h-full w-full'>
+            <StatusBar style='light' />
             <Image className='h-full w-full absolute' source={require('../assets/images/background.png')} />
         </View>
     )
