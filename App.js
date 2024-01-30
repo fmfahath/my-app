c72802a201ee3562263d3ce05a4d318106d3c6d6
fmfahath <fmfahath.dev@gmail.com>
2024-01-30 17:56:13 +0530

    loginScreen/SignupScreen's function components added && rout added in App.js

--- App.js
+++ App.js
@@ -4,22 +4,18 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="signUp" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
