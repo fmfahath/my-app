a370bba13423f854fa0cb779ea654b40999e56ea
fmfahath <fmfahath.dev@gmail.com>
2024-01-30 18:04:57 +0530

    loginScreen : light immage added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,6 +7,12 @@ export default function LoginScreen() {
         <View className='bg-white h-full w-full'>
             <StatusBar style='light' />
             <Image className='h-full w-full absolute' source={require('../assets/images/background.png')} />
+
+            {/* lights */}
+            <View className='flex-row justify-around w-full h-full'>
+                <Image className='h-[225] w-[90]' source={require('../assets/images/light.png')} />
+                <Image className='h-[160] w-[65]' source={require('../assets/images/light.png')} />
+            </View>
         </View>
     )
 }
