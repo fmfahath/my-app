8b91a58376ad428e2f12ec3344437169e2c12f44
fmfahath <fmfahath.dev@gmail.com>
2024-01-30 18:00:07 +0530

    loginScreen : bg imag added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
     return (
-        <View>
-            <Text>LoginScreen</Text>
+        <View className='bg-white h-full w-full'>
+            <Image className='h-full w-full absolute' source={require('../assets/images/background.png')} />
         </View>
     )
 }
